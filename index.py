2bcb374098c8a490f6c387f4435517386f4bc321
Jason <jhughes3818@gmail.com>
2023-03-16 19:45:42 +1100

    remove ff1

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask
-import fastf1 as ff1
+# import fastf1 as ff1
 
-ff1.Cache.enable_cache('cache')
+# ff1.Cache.enable_cache('cache')
 
 
 app = Flask(__name__)
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    session = ff1.get_session(2023, 'Bahrain Grand Prix', 'Q')
-    session.load()
-    ver_lap = session.laps.pick_driver('VER').pick_fastest()
-    print(ver_lap['Time'])
-    return str(ver_lap['Time'])
+    # session = ff1.get_session(2023, 'Bahrain Grand Prix', 'Q')
+    # session.load()
+    # ver_lap = session.laps.pick_driver('VER').pick_fastest()
+    # print(ver_lap['Time'])
+    return "Hello World"
 
 
 @app.route('/about')
