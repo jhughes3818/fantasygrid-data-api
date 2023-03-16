2f188f848c7f53d319d3b604b41f3a75cf3438dc
Jason <jhughes3818@gmail.com>
2023-03-16 19:32:17 +1100

    first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import fastf1 as ff1
+
+ff1.Cache.enable_cache('cache')
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    session = ff1.get_session(2023, 'Bahrain Grand Prix', 'Q')
+    session.load()
+    ver_lap = session.laps.pick_driver('VER').pick_fastest()
+    print(ver_lap['Time'])
+    return str(ver_lap['Time'])
+
+
+@app.route('/about')
+def about():
+    return 'About'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
