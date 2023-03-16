17f6b3ea599c53ba4084c4ecfc6210502385b6fa
Jason <jhughes3818@gmail.com>
2023-03-16 19:47:09 +1100

    add ff1

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask
-# import fastf1 as ff1
+import fastf1 as ff1
 
 # ff1.Cache.enable_cache('cache')
 
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # session = ff1.get_session(2023, 'Bahrain Grand Prix', 'Q')
-    # session.load()
-    # ver_lap = session.laps.pick_driver('VER').pick_fastest()
-    # print(ver_lap['Time'])
-    return "Hello World"
+    session = ff1.get_session(2023, 'Bahrain Grand Prix', 'Q')
+    session.load()
+    ver_lap = session.laps.pick_driver('VER').pick_fastest()
+    print(ver_lap['Time'])
+    return str(ver_lap['Time'])
 
 
 @app.route('/about')
